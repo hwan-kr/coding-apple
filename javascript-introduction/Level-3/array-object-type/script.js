f2f29fff0a573538d38b9f85c6e79e689e6ff1a1
HwanKR <fabronjeon@naver.com>
2024-04-07 19:40:39 +0900

    자바스크립트로 html 생성하는 법

--- javascript-introduction/Level-3/array-object-type/script.js
+++ javascript-introduction/Level-3/array-object-type/script.js
@@ -34,3 +34,28 @@ let car2 = { name: "소나타", price: [50000, 3000, 4000] };
 
 document.getElementById("product").textContent = car2.name;
 document.getElementById("price").textContent = car2.price[0];
+
+const selectValue = document.querySelector(".options");
+const sizeForm = document.querySelector(".product-size");
+
+const shirtsValue =
+    '<option value="">95</option><option value="">100</option><option value="">105</option>';
+
+const pantsValue = '<option value="">28</option><option value="">30</option>';
+
+function shirtsSizeHandler() {
+    if (selectValue.value == "shirts") {
+        sizeForm.style.display = "block";
+        sizeForm.innerHTML = shirtsValue;
+    } else if (selectValue.value == "pants") {
+        sizeForm.style.display = "block";
+        sizeForm.innerHTML = pantsValue;
+    } else {
+        sizeForm.style.display = "none";
+    }
+}
+
+selectValue.addEventListener("input", shirtsSizeHandler);
+
+// let template = "<p>안녕</p>";
+// document.querySelector("#test").insertAdjacentHTML("beforeend", template);
