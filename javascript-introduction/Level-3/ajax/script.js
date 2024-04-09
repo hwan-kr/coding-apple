6ca1c44af3086a9b997a74e1b7cefe50048a69f0
HwanKR <fabronjeon@naver.com>
2024-04-09 21:32:10 +0900

    코딩애플 Ajax 더보기 추가 숙제 해야 함

--- javascript-introduction/Level-3/ajax/script.js
+++ javascript-introduction/Level-3/ajax/script.js
@@ -3,3 +3,32 @@ let products = [
     { id: 1, price: 50000, title: "Springfield Shirt" },
     { id: 2, price: 60000, title: "Black Monastery" },
 ];
+
+const cardGroup = document.querySelector(".card");
+
+for (let i = 0; i < products.length; i++) {
+    let template = `<div class="col-sm-4">
+        <img src="https://via.placeholder.com/600" class="w-100" />
+        <h5>${products[i].title}</h5>
+        <p>가격 : ${products[i].price}</p>
+    </div>`;
+    cardGroup.insertAdjacentHTML("beforeend", template);
+}
+
+const moreBtnElement = document.getElementById("more");
+
+moreBtnElement.addEventListener("click", function () {
+    fetch("https://codingapple1.github.io/js/more1.json")
+        .then((res) => res.json())
+        .then(function (data) {
+            console.log(data);
+            data.forEach(function (item, i) {
+                let template = `<div class="col-sm-4">
+                     <img src="https://via.placeholder.com/600" class="w-100" />
+                    <h5>${data[i].title}</h5>
+                    <p>가격 : ${data[i].price}</p>
+                </div>`;
+                cardGroup.insertAdjacentHTML("beforeend", template);
+            });
+        });
+});
