657994bf34aee9e6ee1269e2ab0cb4a01c8dbf49
HwanKR <fabronjeon@naver.com>
2024-05-23 21:47:35 +0900

    코딩애플 react Part 1  리액트에서 레이아웃 만들 때 쓰는 JSX 문법 3개 완료

---
+++ react/blog/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    let post = "강남 우동 맛집";
+
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4>블로그임</h4>
+            </div>
+            <h4>{post}</h4>
+        </div>
+    );
+}
+
+<div style={{ color: red }}></div>;
+
+export default App;
