09e4733baa940e838040af3d9ccf4698ecf51184
HwanKR <fabronjeon@naver.com>
2024-06-30 21:24:08 +0900

    코딩애플 리액트 map 완료 (연습문제 해야 함)

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -1,19 +1,90 @@
+/* eslint-disable */
+
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-    let post = "강남 우동 맛집";
+    let [title, setTitle] = useState([
+        "남자 코트 추천",
+        "강남 우동 맛집",
+        "파이썬 독학",
+    ]);
+    let [like, setLike] = useState(0);
+    let [modal, setModal] = useState(false);
 
     return (
         <div className="App">
             <div className="black-nav">
-                <h4>블로그임</h4>
+                <h4>ReactBlog</h4>
             </div>
-            <h4>{post}</h4>
+            <button
+                onClick={() => {
+                    let sortedTitle = title.sort();
+                    console.log(sortedTitle);
+                    setTitle(sortedTitle);
+                }}
+                style={{ marginTop: 10 }}
+            >
+                가나다순 정렬
+            </button>
+            <div className="list">
+                <h4>
+                    {title[0]}{" "}
+                    <span
+                        onClick={() => {
+                            setLike(like + 1);
+                        }}
+                    >
+                        👍
+                    </span>{" "}
+                    {like}{" "}
+                    <button
+                        onClick={() => {
+                            let copy = [...title];
+                            copy[0] = "여자 코트 추천";
+                            setTitle(copy);
+                        }}
+                    >
+                        글 수정
+                    </button>
+                </h4>
+                <p>6월 23일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{title[1]}</h4>
+                <p>6월 23일 발행</p>
+            </div>
+            <div className="list">
+                <h4
+                    onClick={() => {
+                        // if (modal == false) {
+                        //     setModal(true);
+                        // } else {
+                        //     setModal(false);
+                        // }
+                        // modal == false ? setModal(true) : setModal(false);
+                        setModal(!modal);
+                    }}
+                >
+                    {title[2]}
+                </h4>
+                <p>6월 23일 발행</p>
+            </div>
+
+            {modal == true ? <Modal /> : null}
         </div>
     );
 }
 
-<div style={{ color: red }}></div>;
+function Modal() {
+    return (
+        <div className="modal">
+            <h4>제목</h4>
+            <p>날짜</p>
+            <p>상세내용</p>
+        </div>
+    );
+}
 
 export default App;
