3f1b384c5f84abdd23672c8a4b8e4a441feb9714
Jeon Seung Hwan <fabronjeon@naver.com>
2024-07-01 16:29:37 +0900

    코딩애플 리엑트 Part1 끝

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -10,79 +10,106 @@ function App() {
         "강남 우동 맛집",
         "파이썬 독학",
     ]);
-    let [like, setLike] = useState(0);
+    let [date, setDate] = useState([
+        "6월 28일 발행",
+        "6월 29일 발행",
+        "6월 30일 발행",
+    ]);
+    let [like, setLike] = useState([0, 0, 0]);
     let [modal, setModal] = useState(false);
-
+    let [index, setIndex] = useState(0);
+    let [inputValue, setInputValue] = useState("");
     return (
         <div className="App">
             <div className="black-nav">
                 <h4>ReactBlog</h4>
             </div>
-            <button
-                onClick={() => {
-                    let sortedTitle = title.sort();
-                    console.log(sortedTitle);
-                    setTitle(sortedTitle);
-                }}
-                style={{ marginTop: 10 }}
-            >
-                가나다순 정렬
-            </button>
-            <div className="list">
-                <h4>
-                    {title[0]}{" "}
-                    <span
-                        onClick={() => {
-                            setLike(like + 1);
-                        }}
-                    >
-                        👍
-                    </span>{" "}
-                    {like}{" "}
-                    <button
-                        onClick={() => {
-                            let copy = [...title];
-                            copy[0] = "여자 코트 추천";
-                            setTitle(copy);
-                        }}
-                    >
-                        글 수정
-                    </button>
-                </h4>
-                <p>6월 23일 발행</p>
-            </div>
-            <div className="list">
-                <h4>{title[1]}</h4>
-                <p>6월 23일 발행</p>
-            </div>
-            <div className="list">
-                <h4
-                    onClick={() => {
-                        // if (modal == false) {
-                        //     setModal(true);
-                        // } else {
-                        //     setModal(false);
-                        // }
-                        // modal == false ? setModal(true) : setModal(false);
-                        setModal(!modal);
-                    }}
-                >
-                    {title[2]}
-                </h4>
-                <p>6월 23일 발행</p>
-            </div>
 
-            {modal == true ? <Modal /> : null}
+            {title.map(function (a, i) {
+                return (
+                    <div className="list" key={i}>
+                        <h4
+                            onClick={() => {
+                                setIndex(i);
+                                setModal(!modal);
+                            }}
+                        >
+                            {title[i]}
+                            <span
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    let newLike = [...like];
+                                    newLike[i] += 1;
+                                    setLike(newLike);
+                                }}
+                            >
+                                👍
+                            </span>{" "}
+                            {like[i]}
+                        </h4>
+                        <p>{date[i]}</p>
+                        <button
+                            onClick={() => {
+                                let copyTitle = [...title];
+                                copyTitle.splice(i, 1);
+                                setTitle(copyTitle);
+                                let copyLike = [...like];
+                                copyLike.splice(i, 1);
+                                setLike(copyLike);
+                                let copyDate = [...date];
+                                copyDate.splice(i, 1);
+                                setDate(copyDate);
+                            }}
+                        >
+                            삭제
+                        </button>
+                    </div>
+                );
+            })}
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    if (inputValue.trim() === "") {
+                        return;
+                    } else {
+                        console.log("Submitted value:", inputValue);
+                        setInputValue(""); // input 값을 제출 후 비웁니다.
+                    }
+                    let copyTitle = [...title];
+                    copyTitle.unshift(inputValue);
+                    setTitle(copyTitle);
+                    let copyLike = [...like];
+                    copyLike.unshift(0);
+                    setLike(copyLike);
+                    let copyDate = [...date];
+                    let date1 = new Date();
+                    copyDate.unshift(date1.toString());
+                    setDate(copyDate);
+                    setInputValue("");
+                }}
+            >
+                <input
+                    onChange={(e) => {
+                        setInputValue(e.target.value);
+                    }}
+                ></input>
+                <button type="submit" onClick={(e) => {}}>
+                    글발행
+                </button>
+            </form>
+
+            {modal == true ? <Modal index={index} title={title} /> : null}
         </div>
     );
 }
 
-function Modal() {
+function Modal(props) {
     return (
         <div className="modal">
-            <h4>제목</h4>
+            <h4>{props.title[props.index]}</h4>
             <p>날짜</p>
             <p>상세내용</p>
+            <button>글수정</button>
         </div>
     );
 }
