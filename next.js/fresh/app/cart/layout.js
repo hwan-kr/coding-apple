bf07374747c9b743cd8bde4f1e40458c7d5e4aba
Jeon Seung Hwan <fabronjeon@naver.com>
2024-07-26 15:03:30 +0900

    client/server component. import 문법

--- next.js/fresh/app/cart/layout.js
+++
@@ -1,8 +0,0 @@
-export default function CartLayout({ children }) {
-    return (
-        <div>
-            {children}
-            <p>현대카드 무이자 이벤트 중</p>
-        </div>
-    );
-}
--- next.js/fresh/app/cart/payment/page.js
+++
@@ -1,7 +0,0 @@
-export default function Payment() {
-    return (
-        <div>
-            <h1>결제페이지입니다</h1>
-        </div>
-    );
-}
