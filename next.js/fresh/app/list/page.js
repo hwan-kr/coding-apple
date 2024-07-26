a09d99df7230dbdc73edf13f903f1e1174a75f94
Jeon Seung Hwan <fabronjeon@naver.com>
2024-07-26 18:21:44 +0900

    좋아요 버튼 만들기 2 (array, object state 변경하려면)

--- next.js/fresh/app/list/page.js
+++ next.js/fresh/app/list/page.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export default function List() {
     let 상품 = ["Tomatoes", "Pasta", "Coconut"];
-    let [수량, 수량변경] = useState(0);
+    let [수량, 수량변경] = useState([0, 0, 0]);
 
     return (
         <div>
@@ -14,17 +14,21 @@ export default function List() {
                     <div className="food" key={i}>
                         <img src={`/food${i}.png`} className="food-img"></img>
                         <h4>{a} 40$</h4>
-                        <span> {수량} </span>
+                        <span> {수량[i]} </span>
                         <button
                             onClick={() => {
-                                수량변경(++수량);
+                                let copy = [...수량];
+                                copy[i]++;
+                                수량변경(copy);
                             }}
                         >
                             +
                         </button>
                         <button
                             onClick={() => {
-                                수량변경(--수량);
+                                let copy = [...수량];
+                                copy[i]--;
+                                수량변경(copy);
                             }}
                         >
                             -
