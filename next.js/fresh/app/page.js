13d09e1c9af1a4f2800d7241fecf2392d7a9880f
Jeon Seung Hwan <fabronjeon@naver.com>
2024-07-26 12:44:49 +0900

    페이지 레이아웃 만들기 (React 기초문법)

--- next.js/fresh/app/page.js
+++ next.js/fresh/app/page.js
@@ -1,3 +1,11 @@
 export default function Home() {
-    return <div></div>;
+    let name = "jeon";
+    return (
+        <div>
+            <h4 style={{ color: "red", fontSize: "30px" }} className="title">
+                승환후레시
+            </h4>
+            <p className="title-sub">by dev {name}</p>
+        </div>
+    );
 }
