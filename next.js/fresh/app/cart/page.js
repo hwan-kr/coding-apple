a4b5bc4518532138bf051d7627dae00cb842b711
Jeon Seung Hwan <fabronjeon@naver.com>
2024-07-26 17:40:43 +0900

    Component에 데이터 전해주려면 props

--- next.js/fresh/app/cart/page.js
+++ next.js/fresh/app/cart/page.js
@@ -1,22 +1,35 @@
 "use client";
 
-import Sayhi from "./sayhi.js";
-
 export default function Cart() {
+    let 장바구니 = ["Tomatoes", "Pasta"];
+
     return (
         <div>
             <h4 className="title">Cart</h4>
-            <CartItem> </CartItem>
-            <CartItem></CartItem>
-            <Sayhi></Sayhi>
+            <CartItem 장바구니={장바구니[0]}></CartItem>
+            <CartItem 장바구니={장바구니[1]}></CartItem>
+            <Banner content="현대카드"></Banner>
+            <ColorBtn color="blue"></ColorBtn>
         </div>
     );
 }
 
-function CartItem() {
+function Banner({ content }) {
+    return <h5>{content} 결제 행사중</h5>;
+}
+
+function ColorBtn({ color }) {
+    return (
+        <div>
+            <button style={{ background: color }}>버튼</button>
+        </div>
+    );
+}
+
+function CartItem({ 장바구니 }) {
     return (
         <div className="cart-item">
-            <p>상품명</p>
+            <p>{장바구니}</p>
             <p>$40</p>
             <p>1개</p>
         </div>
