18e483fcb7b03a4a854596440bd0858fd0dbbecf
Jeon Seung Hwan <fabronjeon@naver.com>
2024-07-26 18:01:38 +0900

    좋아요 버튼 만들기 2 (useState, onClick)

--- next.js/fresh/app/list/page.js
+++ next.js/fresh/app/list/page.js
@@ -1,23 +1,34 @@
+"use client";
+
+import { useState } from "react";
+
 export default function List() {
     let 상품 = ["Tomatoes", "Pasta", "Coconut"];
+    let [수량, 수량변경] = useState(0);
 
     return (
         <div>
             <h4 className="title">상품목록</h4>
-            {/* <div className="food">
-                <h4>{상품[0]} $40</h4>
-            </div>
-            <div className="food">
-                <h4>{상품[1]} $40</h4>
-            </div>
-            <div className="food">
-                <h4>{상품[2]} $40</h4>
-            </div> */}
             {상품.map((a, i) => {
                 return (
                     <div className="food" key={i}>
                         <img src={`/food${i}.png`} className="food-img"></img>
                         <h4>{a} 40$</h4>
+                        <span> {수량} </span>
+                        <button
+                            onClick={() => {
+                                수량변경(++수량);
+                            }}
+                        >
+                            +
+                        </button>
+                        <button
+                            onClick={() => {
+                                수량변경(--수량);
+                            }}
+                        >
+                            -
+                        </button>
                     </div>
                 );
             })}
