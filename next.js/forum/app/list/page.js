fa0c080ddd2701cef74a9b32632b1de3cbe61e1e
Jeon Seung Hwan <fabronjeon@naver.com>
2024-08-06 13:19:46 +0900

    static rendering/dynamic rendering/cache

--- next.js/forum/app/list/page.js
+++ next.js/forum/app/list/page.js
@@ -1,6 +1,7 @@
 import { connectDB } from "@/util/database";
-
 import ListItem from "./Listitem";
+
+export const dynamic = "force-dynamic";
 
 export default async function List() {
     const db = (await connectDB).db("forum");
