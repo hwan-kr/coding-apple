1d4aeec94306eed9d16d090306b945933f81afa4
HwanKR <fabronjeon@naver.com>
2024-08-21 18:52:44 +0900

    삭제기능 만들기 3 (query string / URL parameter)

--- next.js/forum2/app/list/ListItem.js
+++ next.js/forum2/app/list/ListItem.js
@@ -13,25 +13,28 @@ export default function ListItem({ result }) {
                             </Link>
                             <Link href={`/edit/${a._id}`}>✏️</Link>
                             <span
-                                onClick={() => {
-                                    fetch("/api/post/delete", {
-                                        method: "DELETE",
-                                        body: a._id,
-                                    })
-                                        .then((r) => {
-                                            if (r.status == 200) {
-                                                return r.json();
-                                            } else {
-                                                //서버가 에러코드전송시 실행할코드
-                                            }
-                                        })
-                                        .then((result) => {
-                                            //성공시 실행할코드
-                                        })
-                                        .catch((error) => {
-                                            //인터넷문제 등으로 실패시 실행할코드
-                                            console.log(error);
-                                        });
+                                onClick={(e) => {
+                                    // fetch("/api/post/delete", {
+                                    //     method: "DELETE",
+                                    //     body: a._id,
+                                    // })
+                                    //     .then((r) => {
+                                    //         return r.json();
+                                    //     })
+                                    //     .then(() => {
+                                    //         e.target.parentElement.style.opacity = 0;
+                                    //         setTimeout(() => {
+                                    //             e.target.parentElement.style.display =
+                                    //                 "none";
+                                    //         }, 1000);
+                                    //     });
+                                    fetch(`/api/abc/${a._id}`).then(() => {
+                                        e.target.parentElement.style.opacity = 0;
+                                        setTimeout(() => {
+                                            e.target.parentElement.style.display =
+                                                "none";
+                                        }, 1000);
+                                    });
                                 }}
                             >
                                 🗑️
