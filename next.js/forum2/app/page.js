cdf99b7de5b92e32036be9783aca08f154c50b30
HwanKR <fabronjeon@naver.com>
2024-08-25 19:43:14 +0900

    static rendering, dynamic rendering, cache

--- next.js/forum2/app/page.js
+++ next.js/forum2/app/page.js
@@ -1,5 +1,7 @@
 import { connectDB } from "@/util/database";
 
+export const revalidate = 60;
+
 export default async function Home() {
     const db = (await connectDB).db("forum2");
     let result = await db.collection("post").find().toArray();
