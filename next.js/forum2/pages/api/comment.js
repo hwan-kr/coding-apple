4032357b08145c4afeeab139667b4775bf37cdae
HwanKR <fabronjeon@naver.com>
2024-09-01 21:21:25 +0900

    댓글기능 만들기 1 (input 데이터 다루기) 완료

--- next.js/forum2/pages/api/comment.js
+++ next.js/forum2/pages/api/comment.js
@@ -1,16 +1,19 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./auth/[...nextauth]";
+import { connectDB } from "@/util/database";
 
 export default async function handler(요청, 응답) {
     let session = await getServerSession(요청, 응답, authOptions);
     console.log(session);
+    console.log(요청.body);
     const db = (await connectDB).db("forum2");
-
     if (요청.method == "POST") {
-        if (요청.body.title.trim() == "") {
-            return 응답.status(200).json("제목을 비우지 마세요");
+        if (요청.body.trim() == "") {
+            return 응답.status(200).json("댓글이 비어있습니다.");
         }
-        let result = await db.collection("post").insertOne(요청.body);
-        return 응답.status(200).redirect("/list");
+        요청.body = { parent: session.user.email, comment: 요청.body };
+        console.log(요청.body);
+        let result = await db.collection("comment").insertOne(요청.body);
+        return 응답.status(500).json("작성 완료");
     }
 }
