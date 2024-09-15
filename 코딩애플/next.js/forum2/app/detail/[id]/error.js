74c8e0e739f2782a5fd8df1ac6f70b299821c65e
HwanKR <fabronjeon@naver.com>
2024-09-15 16:04:54 +0900

    loading.js / error.js / not-found.js 완료

---
+++ 코딩애플/next.js/forum2/app/detail/[id]/error.js
@@ -0,0 +1,18 @@
+'use client';
+
+export default function Error({ error, reset }) {
+    return (
+        <>
+            <div>
+                <h4>Error</h4>
+                <button
+                    onClick={() => {
+                        reset();
+                    }}
+                >
+                    버튼
+                </button>
+            </div>
+        </>
+    );
+}
--- 코딩애플/next.js/forum2/app/detail/[id]/page.js
+++ 코딩애플/next.js/forum2/app/detail/[id]/page.js
@@ -1,20 +1,26 @@
 import { connectDB } from '@/util/database';
 import { ObjectId } from 'mongodb';
 import Comment from './Comment';
+import { notFound } from 'next/navigation';
 
 export default async function Detail(props) {
     const db = (await connectDB).db('forum2');
     let result = await db
         .collection('post')
         .findOne({ _id: new ObjectId(props.params.id) });
-    return (
-        <>
-            <div>
-                <h4>상세 페이지</h4>
-                <h4>{result.title}</h4>
-                <p>{result.content}</p>
-                <Comment parent={JSON.stringify(result._id)}></Comment>
-            </div>
-        </>
-    );
+
+    if (result == null) {
+        return notFound();
+    } else {
+        return (
+            <>
+                <div>
+                    <h4>상세 페이지</h4>
+                    <h4>{result.title}</h4>
+                    <p>{result.content}</p>
+                    <Comment parent={JSON.stringify(result._id)}></Comment>
+                </div>
+            </>
+        );
+    }
 }
